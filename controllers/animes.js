1cbff20da050d00c280d1e6d50cb873a3985320c
russellasagna <laganzon.r2@yahoo.com>
2022-07-25 20:10:52 -0400

    Update animes MVC, CSS

--- controllers/animes.js
+++ controllers/animes.js
@@ -7,6 +7,10 @@ module.exports = {
 
 function index(req, res) {
     Anime.find({}, function (err, animes) {
+        // req.body.user = req.user._id;
+        // req.body.userName = req.user.name;
+        // req.body.userAvatar = req.user.avatar;
+        // animes.push(req.body);
         res.render('animes/home', { 
             title: 'raMEN', 
             image: 'images/raMEN.png',
--- models/anime.js
+++ models/anime.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const animeSchema = new Schema({
-    anime: {
-        type: String,
-    },
+    anime: { type: String },
+    user: {type: Schema.Types.ObjectId, ref: 'User'},
+    userName: String,
+    userAvatar: String
 });
 
 module.exports = mongoose.model('Anime', animeSchema);
